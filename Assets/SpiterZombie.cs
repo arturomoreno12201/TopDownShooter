d769295ff9bcada7fd1413393dcf533914bc9404
arturomoreno12201 <arturomoreno151@gmail.com>
2018-05-31 14:41:06 -0700

    we got that good good spiter

--- Assets/SpiterZombie.cs
+++ Assets/SpiterZombie.cs
@@ -5,37 +5,40 @@ using UnityEngine.AI;
 
 public class SpiterZombie : MonoBehaviour {
 
+    public Transform target;
     public float Range;
     public string targetsTag = "Player";
-    public Transform target;
+
+    public Transform Rpart;
+    public float tSpeed = 5;
 
     NavMeshAgent nav;
 
     // Use this for initialization
     void Start()
     {
-
+        InvokeRepeating("UpdateTarget", 0, 0.5f);
         target = PlayerManager.instance.Player.transform;
 
         nav = GetComponent<NavMeshAgent>();
-        InvokeRepeating("UpDateTarget",0,0.5f);
+        
     }
 
 
-    void UpDateTarget()
+    void UpdateTarget()
     {
-        GameObject[] targets = GameObject.FindGameObjectsWithTag(targetsTag);
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Player");
         float shortD = Mathf.Infinity;
         GameObject nearest = null;
 
-        foreach (GameObject target in targets)
+        foreach (GameObject targe in targets)
         {
-            float distarget = Vector3.Distance(transform.position, target.transform.position);
+            float distarget = Vector3.Distance(transform.position, targe.transform.position);
 
             if (distarget < shortD)
             {
                 shortD = distarget;
-                nearest = target;
+                nearest = targe;
             }
         }
         if (nearest != null && shortD <= Range)
@@ -54,11 +57,16 @@ public class SpiterZombie : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        
-        nav.SetDestination(target.position);
 
-        if (target = null)
+        
+
+        if (target == null)
             return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookrot = Quaternion.LookRotation(dir);
+        Vector3 rot = Quaternion.Lerp(Rpart.rotation, lookrot, Time.deltaTime * tSpeed).eulerAngles;
+        Rpart.rotation = Quaternion.Euler(0f, rot.y, 0f);
 
     }
 
