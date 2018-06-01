dae4a7db45281fb874d5ccd6a6861a82d46fc0f1
arturomoreno12201 <arturomoreno151@gmail.com>
2018-05-31 23:21:09 -0700

    made a death timer

---
+++ Assets/Scripts/destroyTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyTimer : MonoBehaviour {
+
+    public float time;
+    public float timer = 1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        if (time <= 0)
+        {
+            Destroy(gameObject);
+            time = 1 / timer;
+        }
+
+        time -= Time.deltaTime;
+    }
+}
