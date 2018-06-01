5a22a502acc0a8561b6844c05df3c595364562cc
arturomoreno12201 <arturomoreno151@gmail.com>
2018-05-31 17:30:32 -0700

    I will not sin again against Henry

---
+++ Assets/Scripts/SpitBall.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitBall : MonoBehaviour {
+
+    public Transform tar;
+    public float speed;
+    public GameObject splatter;
+
+private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            Instantiate(splatter, transform.position, transform.rotation);
+        }
+    }
+    public void Seek(Transform _target)
+    {
+
+        tar = _target;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (tar == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = tar.position - transform.position;
+        float disthisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= disthisFrame)
+        {
+
+            //HitTarget();
+            return;
+
+        }
+        transform.Translate(dir.normalized * disthisFrame, Space.World);
+    }
+
+    
+
+}
---
+++ Assets/Scripts/SpiterZombie.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiterZombie : MonoBehaviour {
+
+    public Transform target;
+    public float Range;
+    public string targetsTag = "Player";
+    public float firecount = 0;
+    public float fireRate = 1;
+    public float timer;
+    public Transform Rpart;
+    public float tSpeed = 5;
+
+    NavMeshAgent nav;
+
+    public GameObject Spit;
+    public Transform firePoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0, 0.5f);
+        target = PlayerManager.instance.Player.transform;
+
+        nav = GetComponent<NavMeshAgent>();
+        
+    }
+
+
+    void UpdateTarget()
+    {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Player");
+        float shortD = Mathf.Infinity;
+        GameObject nearest = null;
+
+        foreach (GameObject targe in targets)
+        {
+            float distarget = Vector3.Distance(transform.position, targe.transform.position);
+
+            if (distarget < shortD)
+            {
+                shortD = distarget;
+                nearest = targe;
+            }
+        }
+        if (nearest != null && shortD <= Range)
+        {
+
+            target = nearest.transform;
+
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookrot = Quaternion.LookRotation(dir);
+        Vector3 rot = Quaternion.Lerp(Rpart.rotation, lookrot, Time.deltaTime * tSpeed).eulerAngles;
+        Rpart.rotation = Quaternion.Euler(0f, rot.y, 0f);
+
+        if (timer <= 0f)
+        {
+
+            shoot();
+            timer = 1 / fireRate;
+        }
+        timer -= Time.deltaTime;
+    }
+    void shoot()
+    {
+
+       GameObject SpitGo = (GameObject) Instantiate(Spit, firePoint.position, firePoint.rotation);
+        SpitBall spt = SpitGo.GetComponent<SpitBall>();
+
+        if (spt != null)
+            spt.Seek(target);
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
