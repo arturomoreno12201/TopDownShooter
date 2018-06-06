6a873ff93b1ef55166a471df0e847bc408ead061
arturomoreno12201 <arturomoreno151@gmail.com>
2018-06-06 11:33:35 -0700

    got some more stuff

--- Assets/Scripts/FirendShoot.cs
+++ Assets/Scripts/FirendShoot.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class FirendShoot : MonoBehaviour {
 
+    UnityEngine.AI.NavMeshAgent nav;
+    private EnemyHealth targetEnemy;
+
+    
+
+    [Header ("bullet and shoot things")]
     public Transform target;
     public float Range;
     public string targetsTag = "Enemy";
@@ -12,11 +18,18 @@ public class FirendShoot : MonoBehaviour {
     public float timer;
     public Transform Rpart;
     public float tSpeed = 5;
-
-    UnityEngine.AI.NavMeshAgent nav;
-
     public GameObject Spit;
     public Transform firePoint;
+
+    [Header("Laser")]
+    public bool useLaser = false;
+    public int damageOverTime = 30;
+    public float slowAmount = .5f;
+    public LineRenderer lineRenderer;
+    public ParticleSystem impactEffect;
+    public Light impactLight;
+
+
 
     // Use this for initialization
     void Start()
@@ -65,21 +78,73 @@ public class FirendShoot : MonoBehaviour {
 
 
         if (target == null)
-            return;
+        {
+            if (useLaser)
+            {
+                if (lineRenderer.enabled)
+                {
+                    lineRenderer.enabled = false;
+                    impactEffect.Stop();
+                    impactLight.enabled = false;
+                }
+            }
 
+            return;
+        }
+
+        lookAtTarget();
+        if (useLaser)
+        {
+            Laser();
+        }
+        else
+        {
+            if (timer <= 0f)
+            {
+
+                shoot();
+                timer = 1 / fireRate;
+            }
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void LockOnTarget()
+    {
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(Rpart.rotation, lookRotation, Time.deltaTime * tSpeed).eulerAngles;
+        Rpart.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+    void lookAtTarget()
+    {
         Vector3 dir = target.position - transform.position;
         Quaternion lookrot = Quaternion.LookRotation(dir);
         Vector3 rot = Quaternion.Lerp(Rpart.rotation, lookrot, Time.deltaTime * tSpeed).eulerAngles;
         Rpart.rotation = Quaternion.Euler(0f, rot.y, 0f);
-
-        if (timer <= 0f)
-        {
-
-            shoot();
-            timer = 1 / fireRate;
-        }
-        timer -= Time.deltaTime;
     }
+    void Laser()
+    {
+        targetEnemy.TakeDamage(damageOverTime);
+        
+
+        if (!lineRenderer.enabled)
+        {
+            lineRenderer.enabled = true;
+            impactEffect.Play();
+            impactLight.enabled = true;
+        }
+
+        lineRenderer.SetPosition(0, firePoint.position);
+        lineRenderer.SetPosition(1, target.position);
+
+        Vector3 dir = firePoint.position - target.position;
+
+        impactEffect.transform.position = target.position + dir.normalized;
+
+        impactEffect.transform.rotation = Quaternion.LookRotation(dir);
+    }
+
     void shoot()
     {
 
