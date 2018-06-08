ce438905def4a0680d255e8b64f7a5f326c8f7fa
arturomoreno12201 <arturomoreno151@gmail.com>
2018-06-07 17:38:39 -0700

    final tushes

---
+++ Assets/Scripts/objectmanager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectmanager : MonoBehaviour {
+
+    public Transform SpawnPoint;
+
+    public GameObject[] newThing;
+  
+    public string[] stuff;
+
+    string thing = "Holder";
+    GameObject things;
+    GameObject stuffs;
+   
+    int index;
+   
+
+
+    // Use this for initialization
+    void Start () {
+
+        index =     
+
+        things = GameObject.FindGameObjectWithTag(thing);
+        stuffs = GameObject.Find(stuff[index]);
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //things.transform.Rotate(0, 100 * Time.deltaTime, 0);
+       
+         Destroy(GameObject.FindGameObjectWithTag(thing));
+        HiFriend();
+       
+	}
+
+    void HiFriend()
+    {
+        if (GameObject.Find(stuff[index]))
+        {
+            Instantiate(newThing[3], SpawnPoint.position, SpawnPoint.rotation);
+           
+        }
+    }
+}
